fd2abf9dfab6e42b37afccc2825d227a611f693f
fujiyamaorange <kaito.bakuman@gmail.com>
2023-01-09 13:15:19 +0900

    enums section

---
+++ enums/src/main.rs
@@ -0,0 +1,54 @@
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+// struct IpAddr {
+//     kind: IpAddrKind,
+//     address: String,
+// }
+
+enum IpAddr {
+    // 各列挙子に直接データを添付できる
+    // 構造体，Enum，文字列，数値などなんでも埋め込める
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        // メソッドを定義できる
+        println!("=======");
+        println!("{:?}", self);
+        println!("=======");
+    }
+}
+
+fn main() {
+    let v4 = IpAddrKind::V4;
+    let v6 = IpAddrKind::V6;
+
+    // let home = IpAddr {
+    //     kind: IpAddrKind::V4,
+    //     address: String::from("127.0.0.1"),
+    // };
+
+    // let loopback = IpAddr {
+    //     kind: IpAddrKind::V6,
+    //     address: String::from("::1"),
+    // };
+
+    let home = IpAddr::V4(127, 0, 0, 1);
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("Hello, World!"));
+    m.call();
+}
