5dcf27159cee6c78606ba5c8ffa4ccb752d6ce28
fujiyamaorange <kaito.bakuman@gmail.com>
2023-01-11 15:08:40 +0900

    add dbg! macro

--- lifetime/src/main.rs
+++ lifetime/src/main.rs
@@ -41,6 +41,7 @@ fn main() {
         let string2 = "xyz";
         let result = longest(string1.as_str(), string2);
         println!("The longest string is {}", result);
+        dbg!(result);
     }
 
     let string3 = String::from("long string is long");
