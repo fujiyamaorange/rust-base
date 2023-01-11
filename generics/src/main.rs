a44c9e09f3ec2895c4ca1f41de4a7184b5780546
fujiyamaorange <kaito.bakuman@gmail.com>
2023-01-11 13:23:27 +0900

    generics section

---
+++ generics/src/main.rs
@@ -0,0 +1,48 @@
+// 比較可能トレイト
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    // サイズが既知の型ではない(Copyトレイトを実装しない型が来るかも)
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+#[derive(Debug)]
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            // 値がムーブされるので&selfではなくself？
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+}
